7717011602c05e58a2d81b0fa9f0f64c46ae33ab
haoranyu <hy47@cs.washington.edu>
2020-06-03 13:05:50 -0700

    add some change

--- proxy.py
+++ proxy.py
@@ -1,9 +1,8 @@
-#!/usr/bin/env python3
-
 import socket
 import sys
-import _thread
 import threading
+import datetime
+
 
 def connect_client_to_server(client_con, server_con):
     while True:
@@ -12,8 +11,6 @@ def connect_client_to_server(client_con, server_con):
             if len(request) > 0:
                 server_con.send(request)
         except socket.error:
-            print(str(socket.error))
-            print("connect_client_to_server error")
             return
 
 
@@ -24,23 +21,20 @@ def connect_server_to_client(client_con, server_con):
             if len(response) > 0:
                 client_con.send(response)
         except socket.error:
-            print("connect_server_to_client error")
             return
 
 
 def client(client_con, client_addr):
-    data = ''
     try:
         data = client_con.recv(4096).decode(errors='ignore')
         if not data:
             client_con.close()
             return
     except socket.error:
-        print('client error')
         client_con.close()
         return
     headers = data.split('\r\n')
-    print(headers[0])
+    print(str(datetime.datetime.now()) + ' - >>> ' + str(headers[0]))
     request = headers[0].split(' ')[0]
     server_port = -1
     server_ip = ''
@@ -49,7 +43,6 @@ def client(client_con, client_addr):
             hosts = header.split(':')
             if len(hosts) == 3:
                 server_port = int(hosts[2])
-            print("e", hosts[1].strip())
             server_ip = socket.gethostbyname(hosts[1].strip())
             break
     if server_port == -1:
@@ -82,8 +75,6 @@ def client(client_con, client_addr):
 
             sock.close()
             client_con.close()
-            #_thread.start_new_thread(connect_client_to_server, (client_conn, sock))
-            #_thread.start_new_thread(connect_server_to_client, (client_conn, sock))
         except socket.error:
             client_con.send('HTTP/1.0 502 Bad Gateway\r\n\r\n'.encode())
             sock.close()
@@ -95,15 +86,17 @@ def client(client_con, client_addr):
         data = data.replace('HTTP/1.1', 'HTTP/1.0', 1)
         data = data.replace('keep-alive', 'close')
         sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        print("serverip", server_ip)
-        print("server_port", server_port)
-        sock.connect((server_ip, server_port))
-        sock.send(data.encode())
-        while True:
-            response = sock.recv(1024)
-            client_con.send(response)
-            if len(response) < 1024:
-                break
+        try:
+            sock.connect((server_ip, server_port))
+            sock.send(data.encode())
+            while True:
+                response = sock.recv(1024)
+                client_con.send(response)
+                if len(response) < 1024:
+                    break
+        except socket.error:
+            client_con.close()
+            sock.close()
         client_con.close()
         sock.close()
 
@@ -116,16 +109,13 @@ proxy_ip = socket.gethostbyname('localhost')
 proxy_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 proxy_socket.bind((proxy_ip, proxy_port))
 proxy_socket.listen(5)
+print(str(datetime.datetime.now()) + ' - Proxy listening on ' + str(proxy_ip) + ':' + str(proxy_port))
 while True:
     try:
         client_conn, client_addr = proxy_socket.accept()
-        client_thread = threading.Thread(name=str(client_addr), target=client,
-                                         args=(client_conn, client_addr))
+        client_thread = threading.Thread(name=str(client_addr), target=client, args=(client_conn, client_addr))
         client_thread.setDaemon(True)
-
-        # Start the thread
         client_thread.start()
-        #_thread.start_new_thread(client, (client_conn, client_addr))
     except KeyboardInterrupt:
         proxy_socket.close()
         sys.exit()
